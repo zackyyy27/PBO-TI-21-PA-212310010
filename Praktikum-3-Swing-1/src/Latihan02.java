49c451eac238c59f6793bc5a55d0acfe9827c836
zackyyy27 <113814107+zackyyy27@users.noreply.github.com>
2022-12-30 20:56:58 +0700

    Update Latihan02.java

--- Praktikum-3-Swing-1/src/Latihan02.java
+++ Praktikum-3-Swing-1/src/Latihan02.java
@@ -1,20 +1,17 @@
 import javax.swing.JOptionPane;
-
-public class Latihan02 {
-
-	public static void main(String[] args) {
-		Biodata bio = new Biodata();
-		
-		JOptionPane.showMessageDialog(null, "Selamat Datang!");
-		
-		String nama = JOptionPane.showInputDialog(null, "Masukkan nama lengkap Anda:", "Form Nama", JOptionPane.QUESTION_MESSAGE);
-		String usia = JOptionPane.showInputDialog(null, "Masukkan usia Anda:", "Form Usia", JOptionPane.QUESTION_MESSAGE);
-		String alamat = JOptionPane.showInputDialog(null, "Masukkan alamat Anda:", "Form Alamat", JOptionPane.QUESTION_MESSAGE);
-		
-		int pUsia = Integer.parseInt(usia);
-		
-		bio.setData(nama, pUsia, alamat);
-		bio.getData();
+		public class Latihan02 {
+			public static void main(String[] args){
+				Biodata myBio = new Biodata();
+				myBio.setFullname (JOptionPane.showInputDialog(null,"Masukkan nama lengkap Anda:","From nama ",JOptionPane.QUESTION_MESSAGE));
+				
+				myBio.setAge (JOptionPane.showInputDialog(null,"Masukkan usia Anda:","From Umur",JOptionPane.QUESTION_MESSAGE));
+				
+				myBio.setAlamat (JOptionPane.showInputDialog(null,"Masukkan alamat Anda","From alamat",JOptionPane.QUESTION_MESSAGE));
+				
+				JOptionPane.showMessageDialog(null,"nama: " + myBio.getFullname()
+				+ "\nUsia anda:  " + myBio.getAge() 
+				+ "tahun"+ "\nAlamat anda : " 
+				+ myBio.getAlamat()
+		);
 	}
-	
 }
